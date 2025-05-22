d0a21f2a681ee602493a7994c14c9a0fc2ce889d
yxfhy <yxfhy303@gmail.com>
2025-05-22 15:26:27 +0900

    Update install.sh

--- LINUX/install.sh
+++ LINUX/install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# 環境構築スクリプト
+
+
+sudo apt update
+
 # Node.js（推奨: LTS版）
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # Marp CLI
 sudo npm install -g @marp-team/marp-cli
+
+#  marp to pptx need this
+sudo apt install -y chromium-browser
+
